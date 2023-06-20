6f75fae5bd7cfda5bb3401578ddbfa0e8dfe50c4
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-06-20 15:50:05 +1000

    Fix build error

--- src/components/audio-analyzer/levaPlugins/audioFileInput/component.tsx
+++ src/components/audio-analyzer/levaPlugins/audioFileInput/component.tsx
@@ -1,42 +1,43 @@
-import { useCallback } from "react";
-import { useInputContext, Components } from "leva/plugin";
-import type { AudioFileInputProps } from "./types";
-import { useDropzone } from "react-dropzone";
-const { Row, Label } = Components;
-import {
-  DropZone,
-  FileInputContainer,
-  FilePreview,
-  Instructions,
-  Remove,
-} from "./styled";
+import { useCallback } from 'react'
+import { useInputContext, Components } from 'leva/plugin'
+import type { AudioFileInputProps } from './types'
+import { useDropzone } from 'react-dropzone'
+const { Row, Label } = Components
+import { DropZone, FileInputContainer, FilePreview, Instructions, Remove } from './styled'
 
 export function AudioFileInputComponent() {
-  const { label, value, onUpdate, disabled } =
-    useInputContext<AudioFileInputProps>();
+  const { label, value, onUpdate, disabled } = useInputContext<AudioFileInputProps>()
   const onDrop = useCallback(
     (acceptedFiles: string | any[]) => {
-      if (acceptedFiles.length) onUpdate(acceptedFiles[0]);
+      if (acceptedFiles.length) onUpdate(acceptedFiles[0])
     },
     [onUpdate]
-  );
+  )
 
   const clear = useCallback(
     (e: { stopPropagation: () => void }) => {
-      e.stopPropagation();
-      onUpdate(undefined);
+      e.stopPropagation()
+      onUpdate(undefined)
     },
     [onUpdate]
-  );
+  )
 
   const { getRootProps, getInputProps, isDragAccept, open } = useDropzone({
     maxFiles: 1,
-    accept:
-      "audio/,audio/mpeg,audio/ogg,application/ogg,audio/x-aiff,audio/vnd.wav",
+    accept: {
+      'audio/': [
+        'audio/',
+        'audio/mpeg',
+        'audio/ogg',
+        'application/ogg',
+        'audio/x-aiff',
+        'audio/vnd.wav',
+      ],
+    },
     onDrop,
     disabled,
     noClick: true,
-  });
+  })
 
   // TODO fix any in DropZone
   return (
@@ -54,10 +55,10 @@ export function AudioFileInputComponent() {
         ) : (
           <DropZone {...(getRootProps({ isDragAccept }) as any)} onClick={open}>
             <input {...getInputProps()} />
-            <Instructions>{"Click to Upload"}</Instructions>
+            <Instructions>{'Click to Upload'}</Instructions>
           </DropZone>
         )}
       </FileInputContainer>
     </Row>
-  );
+  )
 }
