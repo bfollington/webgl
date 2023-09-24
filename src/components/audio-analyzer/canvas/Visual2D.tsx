ef574fcc9922fee7d223d780162a6f706491c2ef
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-09-24 21:44:17 +1000

    Layered tiles

--- src/components/audio-analyzer/canvas/Visual2D.tsx
+++ src/components/audio-analyzer/canvas/Visual2D.tsx
@@ -168,7 +168,16 @@ const Visual3DCanvas = ({}: Visual2DCanvasProps) => {
           <Container x={16} y={16}>
             {grid.map((row, i) => {
               return row.map((col, j) => {
-                return <Agent x={i} y={j} textures={frames} />
+                return <Agent x={i} y={j} textures={frames} tint={0xcccccc} offset={0} />
+              })
+            })}
+          </Container>
+        )}
+        {frames != null && (
+          <Container x={16} y={16}>
+            {grid.map((row, i) => {
+              return row.map((col, j) => {
+                return <Agent x={i} y={j} textures={frames} tint={0x707070} offset={0.5} />
               })
             })}
           </Container>
@@ -178,14 +187,27 @@ const Visual3DCanvas = ({}: Visual2DCanvasProps) => {
   )
 }
 
-function Agent({ x, y, textures }: { x: number; y: number; textures: PIXI.Texture[] }) {
+function Agent({
+  x,
+  y,
+  textures,
+  offset,
+  tint,
+}: {
+  x: number
+  y: number
+  textures: PIXI.Texture[]
+  offset: number
+  tint: PIXI.ColorSource
+}) {
   const spr = useRef<PIXI.AnimatedSprite>(null)
   useTick((delta) => {
     if (!spr.current) return
     spr.current.currentFrame =
       Math.round(
         Math.abs(
-          Math.cos((x / 20) * y) * 2 + 24 * Math.sin(Date.now() / 10000 + x / 10 + Math.cos(y / 20))
+          Math.cos((x / 20) * y) * 2 +
+            24 * Math.sin(Date.now() / 10000 + offset + x / 10 + Math.cos(y / 20))
         )
       ) % 24
   })
@@ -201,6 +223,7 @@ function Agent({ x, y, textures }: { x: number; y: number; textures: PIXI.Textur
       isPlaying={true}
       initialFrame={Math.round(Math.abs(24 * Math.sin(Date.now() + x + y))) % 24}
       animationSpeed={0}
+      tint={tint}
     />
   )
 }
